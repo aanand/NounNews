a25c44b665fb4e6ac6a4db887159b369a4e697c2
Aanand Prasad <aanand.prasad@gmail.com>
2013-04-18 19:09:39 +0100

    Dummy 'tweet' script

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source :rubygems
 # gem 'wordnet'
 # gem 'wordnet-defaultdb'
 # gem 'activesupport'
+
+gem 'sqlite3'
+gem 'sequel'
 gem 'typhoeus'
 gem 'nokogiri'
 gem 'titlecase'
---
+++ lib/db.rb
@@ -0,0 +1,26 @@
+require 'sequel'
+
+database_url = ENV['DATABASE_URL'] || 'sqlite://data/db.sqlite3'
+DB = Sequel.connect(database_url)
+
+unless DB.table_exists?(:tweets)
+  DB.create_table :tweets do
+    primary_key :id
+    String :twitter_id
+    String :headline_title
+    String :headline_url
+    DateTime :tweeted_at
+  end
+end
+
+Tweet = DB[:tweets]
+
+class << Tweet
+  def has_been_tweeted?(url)
+    filter(headline_url: url).count > 0
+  end
+
+  def store(attrs)
+    insert(attrs)
+  end
+end
